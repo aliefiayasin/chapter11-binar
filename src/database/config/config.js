a7e95cc50032c12f33957bc0a69d4394e974efa2
Alief <aliefiayasin@gmail.com>
2022-11-02 23:29:53 +0100

    user repo test

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -8,6 +8,13 @@ const config = {
     host: process.env.DB_HOST,
     dialect: "postgres",
   },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+  },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
